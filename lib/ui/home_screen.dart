11eab724e109550727fc660ba8fd5dc73b10c127
GFernandesZ <g.f.z2012gfz@gmail.com>
2025-04-20 14:40:39 -0300

    Ajustando dados

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_banco_douro/models/accounts.dart';
+import 'package:flutter_banco_douro/services/account_service.dart';
 import 'package:flutter_banco_douro/ui/styles/colors.dart';
+import 'package:flutter_banco_douro/ui/widgets/account_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -19,6 +22,38 @@ class HomeScreen extends StatelessWidget {
           )
         ],
       ),
+      body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: FutureBuilder(
+            future: AccountService().getAll(),
+            builder: (context, snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.none:
+                  return const Center(child: CircularProgressIndicator());
+                case ConnectionState.waiting:
+                  return const Center(child: CircularProgressIndicator());
+                case ConnectionState.active:
+                  return const Center(child: CircularProgressIndicator());
+                case ConnectionState.done:
+                  {
+                    if (snapshot.data == null || snapshot.data!.isEmpty) {
+                      return Center(child: Text("Nenhuma informação recebida"));
+                    } else {
+                      List<Account> listAccounts = snapshot.data!;
+                      return ListView.builder(
+                        itemCount: listAccounts.length,
+                        itemBuilder: (context, index) {
+                          Account account = listAccounts[index];
+                          return AccountWidget(
+                            account: account,
+                          );
+                        },
+                      );
+                    }
+                  }
+              }
+            },
+          )),
     );
   }
 }
